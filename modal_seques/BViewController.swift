4e0870ad347fb3a541221c47e8cd4d538f4dc729
ruslanvs <ruslanvs@yahoo.com>
2018-03-12 19:58:18 -0700

    Assignment complete

---
+++ modal_seques/BViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BViewController.swift
+//  modal_seques
+//
+//  Created by Ruslan Suvorov on 3/12/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import UIKit
+
+class BViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func dismissButtonClicked(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ modal_seques/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  modal_seques
+//
+//  Created by Ruslan Suvorov on 3/12/18.
+//  Copyright © 2018 Ruslan Suvorov. All rights reserved.
+//
+
+import UIKit
+
+class AViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
